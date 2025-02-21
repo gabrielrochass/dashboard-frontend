0dcdafe733bdde3c85e483ff74dc4f476574364e
gabrielrochass <grss@cin.ufpe.br>
2025-02-21 18:47:38 -0300

    fix: front consome todas as rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,20 @@ function App() {
   const [dados, setDados] = useState([]);
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:8000/companies/")
-      .then(response => {
-        console.log("Dados recebidos:", response.data); // 🔍 Ver no console
-        setDados(response.data);
+    Promise.all([
+      axios.get("http://127.0.0.1:8000/companies/"),
+      axios.get("http://127.0.0.1:8000/employees/"),
+      axios.get("http://127.0.0.1:8000/metrics/")
+    ])
+    .then(([response1, response2, response3]) => {
+      console.log("Companies:", response1.data);
+      console.log("Employees:", response2.data);
+      console.log("Metrics:", response3.data);
+      setDados({
+        companies: response1.data,
+        employees: response2.data,
+        metrics: response3.data
+      });
       })
       .catch(error => {
         console.error("Erro ao buscar dados:", error);
